73423c5567acea04fc230654a93b6978f551bd48
leejy001 <greatness007@naver.com>
2023-05-10 15:59:29 +0900

    modify: type 수정

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,4 +1,7 @@
-export interface CreateTodosRequest {}
+export interface CreateTodoTypes {
+  isChange: boolean;
+  setIsChange: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
 export interface TodoInfo {
   id: number;
