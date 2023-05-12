3ee02614c36f63e042fbcff1f7996d2c717ac0af
leejy001 <greatness007@naver.com>
2023-05-12 10:48:51 +0900

    delete: authApi 주석 삭제

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -3,37 +3,6 @@ import { SignInRequest, SignUpRequest } from "../types/sign";
 import { LocalTokenRepository } from "../utils/LocalTokenRepository";
 import { HttpClient } from "./httpClient";
 
-// const localTokenRepository = new LocalTokenRepository();
-// const httpClient = new HttpClient(localTokenRepository);
-
-// export const signinApi = async (args: SignInRequest): Promise<string> => {
-//   const signInRes = await httpClient.fetch(URL.SIGNIN_URL, {
-//     method: "POST",
-//     body: JSON.stringify(args)
-//   });
-
-//   if (signInRes.ok) {
-//     const signinResponseData = await signInRes.json();
-//     localTokenRepository.save(signinResponseData.access_token);
-//     return "success";
-//   }
-
-//   return "fail";
-// };
-
-// export const signupApi = async (args: SignUpRequest): Promise<string> => {
-//   const signUpRes = await httpClient.fetch(URL.SIGNUP_URL, {
-//     method: "POST",
-//     body: JSON.stringify(args)
-//   });
-
-//   if (signUpRes.ok) {
-//     return "success";
-//   }
-
-//   return "fail";
-// };
-
 export class AuthApi {
   private httpClient: HttpClient;
   private tokenRepository: LocalTokenRepository;
